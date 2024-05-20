3a685dd829c3cf9395809d263bb3271e46c19912
Andrew Bowman <andrewtbowman@gmail.com>
2024-05-20 17:09:32 -0400

    clean up movable methods to use attributes

--- lib/movable.rb
+++ lib/movable.rb
@@ -1,24 +1,23 @@
 module Movable
   def displayPathtoPrincess
     while @mario != @peach
-      move = get_direction(@mario, @peach)
-      move_mario(move)
+      move_mario(get_direction)
       render
       sleep(0.1)
     end
     render
   end
 
-  def get_direction(mario, peach)
-    mario[:col] == peach[:col] ? row_move(mario, peach) : col_move(mario, peach)
+  def get_direction
+    @mario[:col] == @peach[:col] ? row_move : col_move
   end
   
   def col_move(mario, peach)
-    mario[:col] - peach[:col] > 0 ? "LEFT" : "RIGHT"
+    @mario[:col] - @peach[:col] > 0 ? "LEFT" : "RIGHT"
   end
   
   def row_move(mario, peach)
-    mario[:row] - peach[:row] > 0 ? "UP" : "DOWN"
+    @mario[:row] - @peach[:row] > 0 ? "UP" : "DOWN"
   end
 
   def move_mario(move)
